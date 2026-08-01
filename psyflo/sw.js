44f7c018282565ff8b83e35f740d3f40ce80f202
nardus101 <medesigning@me.com>
2026-08-02 01:17:34 +0200

    Remove portrait orientation prompt

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v17';
+const CACHE='psyflo-shell-v18';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
