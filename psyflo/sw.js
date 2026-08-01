1eb2bf1a1fcfbeb7259158e1db36ef8644c69fc3
nardus101 <medesigning@me.com>
2026-08-02 00:39:53 +0200

    Replace tilt with Chroma Motion

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v13';
+const CACHE='psyflo-shell-v14';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
