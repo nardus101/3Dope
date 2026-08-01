989c86114404fc37f31ec111d6b20ff96728e97d
nardus101 <medesigning@me.com>
2026-08-02 01:09:10 +0200

    Add independent screen orientation lock

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v16';
+const CACHE='psyflo-shell-v17';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
