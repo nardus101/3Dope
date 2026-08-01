be64b88446e917247acfb2f75e93e36a43ac25a9
nardus101 <medesigning@me.com>
2026-08-02 00:46:55 +0200

    Stabilize orientation-aware Motion

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v14';
+const CACHE='psyflo-shell-v15';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
