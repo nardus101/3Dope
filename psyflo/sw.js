d174a2baa26d747a0a45c3bab1426ddc0b6d43cb
nardus101 <medesigning@me.com>
2026-08-01 23:44:10 +0200

    Smooth Flo transitions and keep display awake

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v7';
+const CACHE='psyflo-shell-v8';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
