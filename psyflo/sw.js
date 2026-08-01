7e236bdb3401ce0a0d1b4b6e2d3794b942f401fb
nardus101 <medesigning@me.com>
2026-08-02 00:28:00 +0200

    Improve ambient visibility and visual presence

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v12';
+const CACHE='psyflo-shell-v13';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
