d643ed302a4bdc5c4de206ea895107a1b3c66dba
nardus101 <medesigning@me.com>
2026-08-02 00:55:33 +0200

    Keep Motion mode in landscape

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v15';
+const CACHE='psyflo-shell-v16';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
