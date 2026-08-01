9dc9771df79f3fdb24ea67abb58ce3822e6a0726
nardus101 <medesigning@me.com>
2026-08-01 23:07:18 +0200

    Add 16:9 TV fill output

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v4';
+const CACHE='psyflo-shell-v5';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
