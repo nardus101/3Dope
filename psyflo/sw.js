b067c3ae445ac3d87d4f09a12db3d1fa33713a38
nardus101 <medesigning@me.com>
2026-08-02 19:57:39 +0200

    Refine PsyFlo controller workflow

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v20';
+const CACHE='psyflo-shell-v21';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
