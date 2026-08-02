9ff684fb4b5721b80be33d93c52e8973e2862ed5
nardus101 <medesigning@me.com>
2026-08-02 19:37:27 +0200

    Expand PsyFlo visual engine

--- psyflo/sw.js
+++ psyflo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='psyflo-shell-v19';
+const CACHE='psyflo-shell-v20';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
